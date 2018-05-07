1a3e13a949f0bd379fe60cca3f87407bae354077
jintao <jintao@nonobank.com>
2018-05-07 13:56:24 +0800

    git dev

--- eureka-server/src/main/java/com/ss/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ss/EurekaServerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("git 2");
+		System.out.println("git dev");
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
