508d114b8787e380a3bbaa7bab6cf89cdafe793f
jintao <jintao@nonobank.com>
2018-05-07 14:00:12 +0800

    git hhhh

--- eureka-server/src/main/java/com/ss/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ss/EurekaServerApplication.java
@@ -10,6 +10,7 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		System.out.println("git dev");
+		System.out.println("hhhh");
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
